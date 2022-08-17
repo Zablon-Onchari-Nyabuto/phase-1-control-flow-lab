0c0887d12ba841a5424eccd837acb2a76b8156d1
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-08-17 09:45:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+    if(ride <=400){
+      return 'This one is on me!';
+    }
+  else if 
+    (ride >2000 && ride <=2500){
+      return "I will gladly take your thirty bucks.";
+    }
+    else if
+    (ride >=2500){
+      return "No can do.";
+    }
+  
+  }
+// Write your code here!
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+    const destination = city === "NYC"? "Ok, sounds good." : "No go.";
+    return destination;  
+  }
+  ternaryCheckCity("NYC")
+ // Write your code here!
 
-function switchOnCharmFromTip(){
+ function switchOnCharmFromTip(tip){
+    switch (tip){
+      case "generous":
+        return "Thank you so much.";
+  
+        case "not as generous":
+          return "Thank you.";
+  
+          default:
+            return "Bye."
+    }
+    
+  }
   // Write your code here!
-}
